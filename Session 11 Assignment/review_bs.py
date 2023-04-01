4cce7565930123f6cff985992f12a77dfa4281c8
DimpleB0501 <47828390+DimpleB0501@users.noreply.github.com>
2023-04-01 18:51:45 +0530

    Add files via upload

---
+++ Session 11 Assignment/review_bs.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+url = (
+    "https://www.imdb.com/title/tt12844910/reviews/_ajax?ref_=undefined&paginationKey={}"
+)
+key = ""
+data = {"title": [], "review": []}
+
+while True:
+    response = requests.get(url.format(key))
+    soup = BeautifulSoup(response.content, "html.parser")
+    # Find the pagination key
+    pagination_key = soup.find("div", class_="load-more-data")
+    if not pagination_key:
+        break
+
+    # Update the `key` variable in-order to scrape more reviews
+    key = pagination_key["data-key"]
+    for title, review in zip(
+        soup.find_all(class_="title"), soup.find_all(class_="text show-more__control")
+    ):
+        data["title"].append(title.get_text(strip=True))
+        data["review"].append(review.get_text())
+
+df = pd.DataFrame(data)
+#print(df)
+with open("pathan_movie.txt", "w") as f_out:
+    f_out.write("\n".join(df['review']))
